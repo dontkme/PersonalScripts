738078854ed3636912168b9a52f2b7a43440853a
dontkme <hukaining@gamil.com>
2018-05-04 22:38:58 +0800

    fist upload autores for DESeq2 auto write contract res and plotMA

---
+++ RNAseqs/autores.R
@@ -0,0 +1,21 @@
+autores <- function(G,A,B){
+  comtmp <- paste("res",A,"vs",B,".txt",sep="_")
+  compicname <- paste("res",A,"vs",B,"MA",sep="_")
+  comname <- comtmp
+  res <-results(dds,contrast=c(G,A,B),independentFiltering =F)
+  write.table(res,comname,sep="\t")
+  p <- plotMA(res,alpha=0.05,ylim=c(-10,10))
+  pdfname<-paste(compicname,".pdf")
+  pdf(pdfname)
+  plotMA(res,alpha=0.05,ylim=c(-10,10))
+  dev.off()
+  tiffname<-paste(compicname,".tiff")
+  tiff(tiffname)
+  plotMA(res,alpha=0.05,ylim=c(-10,10))
+  dev.off()
+  pngname<-paste(compicname,".png")
+  png(pngname)
+  plotMA(res,alpha=0.05,ylim=c(-10,10))
+  dev.off()
+  return(comname)
+  }
