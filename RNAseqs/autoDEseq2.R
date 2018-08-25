537c2534cd11a3acb56148922e164b1b439f6f79
dontkme <hukaining@gamil.com>
2018-08-25 19:36:31 +0800

    Upload AutoDESeq2.R.

---
+++ RNAseqs/autoDEseq2.R
@@ -0,0 +1,89 @@
+autores <- function(G,A,B){
+  comtmp <- paste("res",A,"vs",B,".txt",sep="_")
+  compicname <- paste("res",A,"vs",B,"MA",sep="_")
+  comname <- comtmp
+  res <-results(dds,contrast=c(G,A,B),independentFiltering =F)
+  write.table(res,comname,sep="\t")
+  p <- plotMA(res,alpha=0.05,ylim=c(-10,10))
+  pdfname<-paste(compicname,".pdf")
+  pdf(pdfname)
+  plotMA(res,alpha=0.05,ylim=c(-10,10))
+  dev.off()
+  tiffname<-paste(compicname,".tiff")
+  tiff(tiffname)
+  plotMA(res,alpha=0.05,ylim=c(-10,10))
+  dev.off()
+  pngname<-paste(compicname,".png")
+  png(pngname)
+  plotMA(res,alpha=0.05,ylim=c(-10,10))
+  dev.off()
+  return(comname)
+  }
+
+autoPCA <- function(n){
+    compicname<-"PCA"
+    write.table(plotPCA(rld,intgroup="Group",ntop=n,returnData=T),"PCA.txt",sep="\t")
+    pcap <- plotPCA(object = rld,intgroup="Group",ntop=n)
+    pdfname<-paste(compicname,".pdf")
+    pdf(pdfname)
+    print(pcap)
+    dev.off()
+    tiffname<-paste(compicname,".tiff")
+    tiff(tiffname)
+    print(pcap)
+    dev.off()
+    pngname<-paste(compicname,".png")
+    png(pngname)
+    print(pcap)
+    dev.off()
+}
+
+
+
+autoDESeq2 <- function(I,C,n){
+library(DESeq2)
+inputfilename <- I
+Allcds <- read.csv(inputfilename,header = T, sep="\t",row.names = 1,stringsAsFactors = F)
+congroupfile <- C
+Congroup <- read.csv(congroupfile,sep="\t",header = T,stringsAsFactors = F)
+uniqCongroup <- unique(Congroup$Group)
+Congroups <- combn(uniqCongroup,2)
+dds <- DESeqDataSetFromMatrix(Allcds,colData = Congroup,design =~ Group)
+dds <- DESeq(dds)
+write.table(counts(dds,normalized=T),"normalizedallCDS.txt",sep="\t")
+write.table(sizeFactors(dds),"sizefactors.txt")
+rld <- rlog(dds,blind=F)
+write.table(cor(Allcds),"corAllcds.txt",sep="\t")
+write.table(cor(counts(dds)),"corcounts.txt",sep="\t")
+library("pheatmap")
+library("RColorBrewer")
+sampleDists <- dist( t( assay(rld) ) )
+sampleDistMatrix <- as.matrix( sampleDists )
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+compicname<-"CorHeatmap"
+pdfname<-paste(compicname,".pdf")
+pdf(pdfname)
+pheatmap(sampleDistMatrix,
+clustering_distance_rows=sampleDists,
+clustering_distance_cols=sampleDists,
+col=colors)
+  dev.off()
+  tiffname<-paste(compicname,".tiff")
+  tiff(tiffname)
+pheatmap(sampleDistMatrix,
+clustering_distance_rows=sampleDists,
+clustering_distance_cols=sampleDists,
+col=colors)
+ dev.off()
+  pngname<-paste(compicname,".png")
+  png(pngname)
+pheatmap(sampleDistMatrix,
+clustering_distance_rows=sampleDists,
+clustering_distance_cols=sampleDists,
+col=colors)
+dev.off()
+autoPCA(n)
+for (i in 1:(length(Congroups)/2)) {
+     autores("Group",Congroups[1,i],Congroups[2,i])
+ }
+}
