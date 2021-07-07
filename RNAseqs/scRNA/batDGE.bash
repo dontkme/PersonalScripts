6383ddbfb830b8795c6cceba7a1736820c3a0fad
dontkme <hukaining@gmail.com>
2021-07-07 11:47:50 -0500

    add batDGE.bash

---
+++ RNAseqs/scRNA/batDGE.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+MG=200
+DropseqPATH=/project2/xczhang/KnHu/biosoft/Drop-seq_tools-2.4.0
+for i in $(ls -d *Res)
+do
+short=${i}.${MG} 
+echo "#!/bin/bash" > $short.sbatch
+echo "#SBATCH --job-name=$short.MG.$MG" >>$short.sbatch
+echo "#SBATCH --output=$short.out" >>$short.sbatch
+echo "#SBATCH --error=$short.err" >>$short.sbatch
+echo "#SBATCH --partition=broadwl" >>$short.sbatch
+echo "#SBATCH --ntasks=1" >>$short.sbatch
+echo "#SBATCH --mem=8G" >>$short.sbatch
+echo "module load java" >>$short.sbatch
+echo "cd $i" >>$short.sbatch
+#echo "java -jar -Xmx8G ~/xczhang/KnHu/biosoft/picard. -F1 $file1 -F2 $file2 -O $short.unmapped.bam -SM $short -RG $short" >>$short.sbatch
+echo "${DropseqPATH}/DigitalExpression -I final.bam -O DGE.MG${MG}.out.txt -SUMMARY DGE.MG${MG}.summary.txt -MIN_NUM_GENES_PER_CELL ${MG}" >>$short.sbatch
+echo "sbatch $short.sbatch"
+done
