35fc0751b0632b9da30b82b4b083098a778e94f8
dontkme <hukaining@gmail.com>
2022-01-14 13:41:49 -0600

    add spliceai sbatch

---
+++ RNAseqs/spliceAI/forspliceai_NMD_ex.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+spliceD=4000
+ref=GRCh38.p13.genome.fa
+spliceA=grch38
+for file in $(ls Test3_*NMD_ex*chr*.vcf)
+do
+
+#echo $file
+chr=$(echo $file|sed  's/.*\(chr.*\)\.vcf/\1/g')
+#echo $chr
+
+outfile="Test3_humanunion_fixbug_rmNMD_GTF.NMD_exAS968.intron_region2.gnomad.${chr}.spliceai.D${spliceD}.vcf"
+#echo $outfile
+echo "#!/bin/bash" >${file}.sbatch
+echo "#SBATCH --job-name=NMD_ex_spliceai.${chr}.D${spliceD}">>${file}.sbatch
+echo "#SBATCH --output=NMD_ex_spliceai.${chr}.D${spliceD}.out">>${file}.sbatch
+echo "#SBATCH --error=NMD_ex_spliceai.${chr}.D${spliceD}.err">>${file}.sbatch
+echo "#SBATCH --nodes=1">>${file}.sbatch
+echo "#SBATCH --partition=gpu2">>${file}.sbatch
+echo "#SBATCH --ntasks=1">>${file}.sbatch
+echo "#SBATCH --gres=gpu:1">>${file}.sbatch
+echo "#SBATCH -w midway2-0664">>${file}.sbatch
+
+echo "source activate /project2/xczhang/KnHu/condaenv/spliceai">>${file}.sbatch
+echo "module load cuda/11.2">>${file}.sbatch
+echo "spliceai -I $file  -O $outfile  -A $spliceA -R $ref -D $spliceD">>${file}.sbatch
+
+
+done
